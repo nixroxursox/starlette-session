3079a9d1b55062d96ae566afebf1894b4ff0e520
carlrdnz <carlrdnz@gmail.com>
2023-08-02 12:07:30 +0800

    Added ScrollToTop Component and Modified App.JS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import AllProducts from './components/AllProducts';
 import ProductDetail from './components/ProductDetail';
 import Categories from './components/Categories';
 import CategoryProducts from './components/CategoryProducts';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <Route path='/category/:category_slug/:category_id' element={<CategoryProducts/>}/>
       <Route path='/product/:product_slug/:product_id' element={<ProductDetail/>}/>
     </Routes>
+    <ScrollToTop/>
     <Footer />
     </>
   );
