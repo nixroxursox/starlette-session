773d2c938d408fc5bb7f2ac621896b8a7688af7f
Rocky Allen <nixrox@gmail.com>
2024-01-27 04:17:52 -0500

    db_DATA

--- db/playground-1.mongodb.js
+++ db/playground-1.mongodb.js
@@ -3,27 +3,18 @@
 // Use Ctrl+Space inside a snippet or a string literal to trigger completions.
 
 // The current database to use.
-use('eCom');
+use('NCKeno');
 
 // Search for documents in the current collection.
-db.getCollection('session_data')
-  .findOne(
+db.getCollection('keno_data')
+  .find(
     {
-      'reg_data.userId': '*'
+      'draw number': '*'
       /*
       * Filter
       * fieldA: value or expression
       */
     },
-    {
-      /*
-      * Projection
-      * _id: 0, // exclude _id
-      * fieldA: 1 // include field
-      */
-     'session_data.session_id': 1,
-     'session_data.metadata.lifetime': 1
-    }
   )
   .sort({
     /*
